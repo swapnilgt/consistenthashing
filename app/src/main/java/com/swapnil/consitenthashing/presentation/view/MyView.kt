f7a3556686d85d08c43d73c8bb3ea65fb543eefe
swapnil.g <swapnilg@zeta.tech>
2024-04-25 23:25:49 +0530

    Buttons added

--- app/src/main/java/com/swapnil/consitenthashing/presentation/view/MyView.kt
+++ app/src/main/java/com/swapnil/consitenthashing/presentation/view/MyView.kt
@@ -1,25 +1,95 @@
 package com.swapnil.consitenthashing.presentation.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.coerceAtMost
 import androidx.compose.ui.unit.dp
 import com.swapnil.consitenthashing.presentation.pojo.NodeViewData
 
 @Composable
 fun MyView(
-    nodeViewData: List<NodeViewData>,
-    requestViewData: List<NodeViewData>,
+    nodeViewData: List<NodeViewData> = emptyList(),
+    requestViewData: List<NodeViewData> = emptyList(),
     borderColor: Color = Color.Red,
     width: Dp = 100.dp
 ) {
     Surface {
-        Ring(
-            borderColor = borderColor,
-            width = width,
-            nodeViewData = nodeViewData,
-            requestViewData = requestViewData
-        )
+        Column {
+            Ring(
+                borderColor = borderColor,
+                width = width,
+                nodeViewData = nodeViewData,
+                requestViewData = requestViewData
+            )
+
+            Row(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(10.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Column(
+                    modifier = Modifier.weight(1f).fillMaxHeight(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Button(
+                        onClick = { /*TODO*/ },
+                        enabled = nodeViewData.isNotEmpty()
+                    ) {
+                        Text(text = "Add Request")
+                    }
+                }
+                Column(
+                    modifier = Modifier.weight(1f).fillMaxHeight(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Button(
+                        onClick = { /*TODO*/ }
+                    ) {
+                        Text(text = "Add Node")
+                    }
+                }
+            }
+        }
     }
+}
+
+@Composable
+@Preview(showSystemUi = true)
+fun MyViewPreview() {
+    val configuration = LocalConfiguration.current
+    val screenHeight = configuration.screenHeightDp.dp
+    val screenWidth = configuration.screenWidthDp.dp
+    MyView(
+        width = screenWidth.coerceAtMost(screenHeight),
+        nodeViewData = mutableListOf(NodeViewData(
+            bkgColor = Color.Red,
+            borderColor = Color.Black,
+            radius = 20.dp,
+            radianVal = 0.0f
+        )),
+        requestViewData = mutableListOf(NodeViewData(
+            bkgColor = Color.Red,
+            borderColor = Color.Black,
+            radius = 20.dp,
+            radianVal = 180.0f
+        ))
+    )
 }
